457fb16f99f0e79930b18ef8a903f5f50359081c
dvanlooy <dvanlooy@DESKTOP-CGIRFOQ.lan>
2016-09-23 07:22:47 +0200

    fzz

--- src/main/java/be/vdab/web/BrouwerController.java
+++ src/main/java/be/vdab/web/BrouwerController.java
@@ -3,17 +3,26 @@ package be.vdab.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import be.vdab.services.BrouwerService;
 
 @Controller
 @RequestMapping("/brouwers")
-class WerknemerController {
+class BrouwerController {
 	private static final String BROUWERS_VIEW = "brouwers/brouwers";
 	private static final String BEGIN_NAAM_VIEW = "brouwers/beginnaam";
 	private static final String TOEVOEGEN_VIEW = "brouwers/toevoegen";
 	
+	private final BrouwerService brouwerService;
+	
+	BrouwerController(BrouwerService brouwerService) {
+		this.brouwerService = brouwerService;
+		}
+	
 	@GetMapping
-	String findAll() {
-		return BROUWERS_VIEW;
+	ModelAndView brouwers() {
+	return new ModelAndView(BROUWERS_VIEW, "brouwers", brouwerService.findAll());
 	}
 	
 	@GetMapping("beginnaam")
